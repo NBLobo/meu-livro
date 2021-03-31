85eb920b5971a226a93bb30f50743eca92da74e2
NBLobo <nblobo@gmail.com>
2021-03-31 17:22:41 -0300

    Commit 1.0.1

--- projeto_livros/settings.py
+++ projeto_livros/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'v3&c(352aw9s8==gu10bwrzralzmuy($*ladx$r-m473(cj%r+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['meu-livro.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
